99625ad359cae9c22b2d9c56bd6a946c106dc8b8
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-03-22 14:01:21 +1100

    Make the latest 20 news better

--- src/http/get-api-news/index.js
+++ src/http/get-api-news/index.js
@@ -2,18 +2,20 @@ const data = require('@begin/data');
 
 
 exports.handler = async function http (req) {
-  const newsStrories = await data.get({table:'news'});
+  const newsStrories = await data.get({table:'news', limit: 20});
   let storiesToShow = [];
 
   for (newsStory of newsStrories) {
     storiesToShow.push(newsStory.story);
   }
+
+  const sortedStories = storiesToShow.sort((a, b) => new Date(b.published_date) - new Date(a.published_date))
   return {
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     statusCode: 200,
-    body: JSON.stringify(storiesToShow)
+    body: JSON.stringify(sortedStories)
   }
 }
