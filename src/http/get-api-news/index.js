980070bab3111193aa80cc440962104edadc7f36
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-03-22 21:39:50 +1100

    Add guardian - better table structure
    - [x] Better error handling in frontend

--- src/http/get-api-news/index.js
+++ src/http/get-api-news/index.js
@@ -1,12 +1,17 @@
 const data = require('@begin/data');
 
+function pick(obj, attributes = []) {
+  return Object.entries(obj)
+    .filter(([key]) => attributes.includes(key))
+    .reduce((obj, [key, val]) => Object.assign(obj, { [key]: val }), {});
+}
 
 exports.handler = async function http (req) {
   const newsStrories = await data.get({table:'news', limit: 20});
   let storiesToShow = [];
 
   for (newsStory of newsStrories) {
-    storiesToShow.push(newsStory.story);
+    storiesToShow.push(pick(newsStory, ['headline', 'url', 'published_date', 'source']));
   }
 
   const sortedStories = storiesToShow.sort((a, b) => new Date(b.published_date) - new Date(a.published_date))
